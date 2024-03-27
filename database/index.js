5f16d3cd7a02c467ffa88239e6968521cab605b7
Felipe Saavedra <fisaavedrae@gmail.com>
2024-03-26 23:53:56 -0300

    readme

--- database/index.js
+++ database/index.js
@@ -5,15 +5,13 @@ const pool = new Pool({
   connectionString: process.env.POSTGRES_URL,
 });*/
 
-
 const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
-  port: 5432,
+  port: process.env.DB_PORT,
   allowExitOnIdle: true,
 });
-
 
 module.exports = pool;
